a0324fe1cbc8eb30e2cb1180488c67e8c4fc5974
NguyenMinhDucIT <55796092+NguyenMinhDucIT@users.noreply.github.com>
2020-12-15 17:09:25 +0700

    Add files via upload

--- BaiTapOOP_w7/src/Inheritance/NhanVien.java
+++ BaiTapOOP_w7/src/Inheritance/NhanVien.java
@@ -1,11 +1,17 @@
 package Inheritance;
 
+import java.util.Scanner;
+
 public class NhanVien {
 	private String tenNhanVien;
 	private double heSoLuong;
 	public static final double LUONG_CO_BAN = 750000;
 	public static final double LUONG_MAX = 20000000;
 	//
+	public NhanVien(String name, double heSoLuong) {
+		setTenNhanVien(name);
+		setHeSoLuong(heSoLuong);
+	}
 	public String getTenNhanVien() {
 		return tenNhanVien;
 	}
@@ -31,6 +37,46 @@ public class NhanVien {
 		System.out.println("He so luong: " + this.heSoLuong);
 		System.out.println("Luong: " + this.tinhLuong());
 		System.out.println("Tang luong: " + this.tangLuong(this.tinhLuong()));
+		System.out.println("*****");
+		
+	}
+	
+//	public NhanVien nhapNV() {
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("Ten nhan vien: ");
+//		String name = sc.nextLine();
+//		sc.nextDouble();
+//		System.out.println("He so luong nhan vien: ");
+//		double hsl = sc.nextDouble();
+//		NhanVien tmp = new NhanVien(name, hsl);
+//		return tmp;
+//		
+//	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		PhongBan pb = new PhongBan("Nhan su");
+		NhanVien nv;
+		for (int i = 0; i < 2; i++) {
+			System.out.println("Nhap ten nhan vien: ");
+			String name = sc.nextLine();
+			System.out.println("Nhap he so luong: ");
+			double hsl = sc.nextDouble();
+			sc.nextLine();
+			nv = new NhanVien(name, hsl);
+			pb.themNV(nv);
+			
+		}
+		
+		pb.inTT();
+		System.out.println("So nhan vien: " + pb.getSoNhanVien());
+		NhanVien nv2 = pb.xoaNV();
+		nv2.inTT();
+		System.out.println("So nhan vien: " + pb.getSoNhanVien());
+		
+			
+			
+		
 		
 	}
 }
--- BaiTapOOP_w7/src/Inheritance/PhongBan.java
+++ BaiTapOOP_w7/src/Inheritance/PhongBan.java
@@ -1,10 +1,17 @@
 package Inheritance;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class PhongBan {
 	private String tenPhongBan;
-	private byte soNhanVien = 0;
+	private byte soNhanVien ;
 	public static final byte SO_NV_MAX = 100;
 	//
+	public PhongBan(String name) {
+		setTenPhongBan(name);
+		
+	}
 	public String getTenPhongBan() {
 		return tenPhongBan;
 	}
@@ -18,7 +25,7 @@ public class PhongBan {
 		this.soNhanVien = soNhanVien;
 	}
 	//
-	public NhanVien[] stackNV;
+	public ArrayList<NhanVien> stackNV = new ArrayList<NhanVien>();
 	public boolean isFull() {
 		if(soNhanVien == SO_NV_MAX) return true;
 		return false;
@@ -32,8 +39,7 @@ public class PhongBan {
 			System.out.println("Số lượng nhân viên đã đủ!");
 			return;
 		}
-		
-		stackNV[soNhanVien] = nv;
+		stackNV.add(soNhanVien, new NhanVien(nv.getTenNhanVien(), nv.getHeSoLuong()));
 		soNhanVien++;
 	}
 	public NhanVien xoaNV() {
@@ -41,27 +47,31 @@ public class PhongBan {
 			System.out.println("Không có nhân viên để xóa!");
 			return null;
 		} else {
-			NhanVien tmp = stackNV[soNhanVien];
-			stackNV[soNhanVien] = null;
+			NhanVien tmp = stackNV.get(soNhanVien - 1);
+			stackNV.set(soNhanVien - 1, null);
 			soNhanVien--;
 			return tmp;
 		}
 	}
 	public double tongLuong() {
 		double sum = 0.0;
-		for (int i = 0; i < stackNV.length; i++) {
-			sum = sum + stackNV[i].tinhLuong();
+		for (int i = 0; i < stackNV.size(); i++) {
+			sum = sum + stackNV.get(i).tinhLuong();
 		}
 		return sum;
 	}
 	public void inTT() {
+		System.out.println("--- Content ---");
 		System.out.println("Ten phong: " + tenPhongBan);
 		System.out.println("So nhan vien: " + soNhanVien);
+		System.out.println("*********");
 		System.out.println("Cac nhan vien:");	
-		for (int i = 0; i < stackNV.length; i++) {
-			stackNV[i].inTT();
+		for (int i = 0; i < stackNV.size(); i++) {
+			stackNV.get(i).inTT();
 		}
 	}
+	
+	
 	
 	
 	
